ee0e1c1af80a18cd228ad0982fa76041243203df
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 16:23:56 -0700

    [complete] about literals

--- src/Koans/AboutLiterals.elm
+++ src/Koans/AboutLiterals.elm
@@ -15,16 +15,16 @@ testSuite =
     "About Literals"
     [ test
         "strings are enclosed in double quotes"
-        (assertEqual "A string" xString)
+        (assertEqual "A string" "A string")
     , test
         "characters are enclosed in single quotes"
-        (assertEqual 'A' xChar)
+        (assertEqual 'A' 'A')
     , test
         "floats have a decimal"
-        (assertEqual 42.24 xNum)
+        (assertEqual 42.24 42.24)
     , test
         "integers do not"
-        (assertEqual 42 xNum)
+        (assertEqual 42 42)
     , test
         "number literals can be integers"
         <| let
@@ -32,7 +32,7 @@ testSuite =
             num =
               42
            in
-            (assertEqual num xNum)
+            (assertEqual num 42)
     , test
         "number literals can be floats"
         <| let
@@ -40,8 +40,8 @@ testSuite =
             num =
               42.0
            in
-            (assertEqual num xNum)
+            (assertEqual num 42.0)
     , test
         "lists are denoted by brackets"
-        (assertEqual [ 1, 2, 3 ] xList)
+        (assertEqual [ 1, 2, 3 ] [ 1, 2, 3 ])
     ]
