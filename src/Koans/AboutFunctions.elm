6f4e8cf5dc4e6b9c738122be3b8ae90b71dc6555
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 16:59:47 -0700

    [complete] about functions

--- src/Koans/AboutFunctions.elm
+++ src/Koans/AboutFunctions.elm
@@ -3,7 +3,7 @@ module AboutFunctions exposing (..)
 import ElmTest exposing (..)
 import TestHelpers exposing (..)
 
-
+add1 : number -> number
 add1 x =
   x + 1
 
@@ -12,7 +12,7 @@ add : number -> number -> number
 add x y =
   x + y
 
-
+subtract : number -> number -> number
 subtract =
   \x y -> x - y
 
@@ -21,7 +21,7 @@ subtractFrom4 : number -> number
 subtractFrom4 =
   subtract 4
 
-
+always5 : a -> number
 always5 =
   always 5
 
@@ -39,41 +39,42 @@ testSuite =
     "About Functions"
     [ test
         "the identity function returns whatever it is passed"
-        (assertEqual (identity 4) xNum)
+        (assertEqual (identity 4) 4)
     , test
         "functions are defined as 'name args = implementation'"
-        (assertEqual 5 (add1 xNum))
+        (assertEqual 5 (add1 4))
     , test
         "functions may have an optional type signature"
-        (assertEqual 5 (add 3 xNum))
+        (assertEqual 5 (add 3 2))
     , test
         "functions may be used as infix with backticks"
-        (assertEqual 5 (3 `add` xNum))
+        (assertEqual 5 (3 `add` 2))
     , test
         "anonymous functions are defined with '\\args -> implementation'"
-        (assertEqual 5 (subtract 8 xNum))
+        (assertEqual 5 (subtract 8 3))
     , test
         "anonymous functions may be defined inline"
-        (assertEqual 5 ((\x y -> x - y) 8 xNum))
+        (assertEqual 5 ((\x y -> x - y) 8 3))
     , test
         "functions passed some values return curried functions"
-        (assertEqual 1 (subtractFrom4 xNum))
+        (assertEqual 1 (subtractFrom4 3))
     , test
         "the always function returns its first argument, no matter what the second is"
-        (assertEqual (always5 4) xNum)
+        (assertEqual (always5 4) 5)
     , test
         "the flip function flips the order of the first two arguments of a function"
-        (assertEqual (subtract4 9) xNum)
+        (assertEqual (subtract4 9) 5)
     , test
         "f <| a applies the function f to the arg a"
-        (assertEqual (subtract4 <| add 1 9) xNum)
+        (assertEqual (subtract4 <| add 1 9) 6)
     , test
         "a |> f applies the function f to the arg a"
-        (assertEqual (add 1 9 |> subtract4) xNum)
+        (assertEqual (add 1 9 |> subtract4) 6)
     , test
         "g << f composes function g with function f"
-        (assertEqual ((multiplyBy2 << subtract4) 9) xNum)
+
+        (assertEqual ((multiplyBy2 << subtract4) 9) 10)
     , test
         "f >> g composes function g with function f"
-        (assertEqual ((subtract4 >> multiplyBy2) 9) xNum)
+        (assertEqual ((subtract4 >> multiplyBy2) 9) 10)
     ]
