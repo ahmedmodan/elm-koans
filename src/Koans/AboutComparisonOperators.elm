61ea3cab9c2f4a19d186ce826bf96b3c408098ad
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 16:24:12 -0700

    [complete] about comparison operators

--- src/Koans/AboutComparisonOperators.elm
+++ src/Koans/AboutComparisonOperators.elm
@@ -9,42 +9,42 @@ testSuite =
     "About Comparison Operators"
     [ test
         "== tests for equality"
-        (assert (1 == xNum))
+        (assert (1 == 1))
     , test
         "/= tests for inequality"
-        (assert (-1 /= xNum))
+        (assert (-1 /= 1))
     , test
         "< tests for less than"
-        (assert (1 < xNum))
+        (assert (1 < 2))
     , test
         "<= tests for less than or equal to"
-        (assert (1 <= xNum))
+        (assert (1 <= 1))
     , test
         "> tests for greater than"
-        (assert (1 > xNum))
+        (assert (1 > 0))
     , test
         ">= tests for greater than or equal to"
-        (assert (1 >= xNum))
+        (assert (1 >= 1))
     , test
         "Floats are comparable"
-        (assert (1.5 >= xNum))
+        (assert (1.5 >= 1.5))
     , test
         "Strings are comparable"
-        (assert ("A string" == xString))
+        (assert ("A string" == "A string"))
     , test
         "Chars are comparable"
-        (assert ('a' == xChar))
+        (assert ('a' == 'a'))
     , test
         "max returns the maximum of two comparables"
-        (assertEqual (max 1 2) xNum)
+        (assertEqual (max 1 2) 2)
     , test
         "min returns the minimum of two comparables"
-        (assertEqual (min 1 2) xNum)
+        (assertEqual (min 1 2) 1)
     , test
         "compare returns an Order"
         -- valid Order values are LT, EQ, and GT
-        (assertEqual (compare 1 2) xOrd)
+        (assertEqual (compare 1 2) LT)
     , test
         "functions can be made infix with `backticks`"
-        (assertEqual (2 `compare` 1) xOrd)
+        (assertEqual (2 `compare` 1) GT)
     ]
