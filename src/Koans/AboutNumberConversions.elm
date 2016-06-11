921dac9eaf65588e9342ea875e6323b21304b719
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 16:41:39 -0700

    [complete] about number conversions

--- src/Koans/AboutNumberConversions.elm
+++ src/Koans/AboutNumberConversions.elm
@@ -9,29 +9,29 @@ testSuite =
     "About Number Conversions"
     [ test
         "toFloat converts an int to a float"
-        (assertEqual (toFloat 5) xNum)
+        (assertEqual (toFloat 5) 5.0)
     , test
         "floor converts a float to an int (rounding down)"
-        (assertEqual (floor 3.6) xNum)
+        (assertEqual (floor 3.6) 3)
     , test
         "floor converts a float to an int (rounding down)"
-        (assertEqual (floor -3.6) xNum)
+        (assertEqual (floor -3.6) -4)
     , test
         "ceiling converts a float to an int (rounding up)"
-        (assertEqual (ceiling 3.6) xNum)
+        (assertEqual (ceiling 3.6) 4)
     , test
         "ceiling converts a float to an int (rounding up)"
-        (assertEqual (ceiling -3.6) xNum)
+        (assertEqual (ceiling -3.6) -3)
     , test
         "round converts a float to an int (rounding to the closest int)"
-        (assertEqual (round 3.6) xNum)
+        (assertEqual (round 3.6) 4)
     , test
         "round converts a float to an int (rounding to the closest int)"
-        (assertEqual (round -3.6) xNum)
+        (assertEqual (round -3.6) -4)
     , test
         "truncate converts a float to an int (rounding towards 0)"
-        (assertEqual (truncate 3.6) xNum)
+        (assertEqual (truncate 3.6) 3)
     , test
         "truncate converts a float to an int (rounding towards 0)"
-        (assertEqual (truncate -3.6) xNum)
+        (assertEqual (truncate -3.6) -3)
     ]
