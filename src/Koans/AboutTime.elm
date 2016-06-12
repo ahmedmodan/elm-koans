aa798fbb689fbeea6ec84f3456eb7e6ed7f4562c
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 19:16:08 -0700

    [complete] about time

--- src/Koans/AboutTime.elm
+++ src/Koans/AboutTime.elm
@@ -10,20 +10,20 @@ testSuite =
     "About Time"
     [ test
         "time is just a Float"
-        (assertEqual 123.45 xTime)
+        (assertEqual 123.45 123.45)
     , test
         "a constant exists for hour"
-        (assertEqual (1 * Time.hour) xTime)
+        (assertEqual (1 * Time.hour) 3600000)
     , test
         "and minute"
-        (assertEqual (1 * Time.minute) xTime)
+        (assertEqual (1 * Time.minute) 60000)
     , test
         "and second"
-        (assertEqual (1 * Time.second) xTime)
+        (assertEqual (1 * Time.second) 1000)
     , test
         "and millisecond"
-        (assertEqual (1 * Time.millisecond) xTime)
+        (assertEqual (1 * Time.millisecond) 1)
     , test
         "helpers exist to convert back to Floats"
-        (assertEqual (Time.inSeconds 1000) xNum)
+        (assertEqual (Time.inSeconds 1000) 1)
     ]
