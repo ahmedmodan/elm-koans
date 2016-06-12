e7e8c95a372d5e30b6d51c10090ac964a7f7c45b
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 18:52:06 -0700

    [complete] about results

--- src/Koans/AboutResults.elm
+++ src/Koans/AboutResults.elm
@@ -10,20 +10,20 @@ testSuite =
     "About Result"
     [ test
         "results represent the result of a function"
-        (assertEqual (Ok 42) xResult)
+        (assertEqual (Ok 42) (Ok 42))
     , test
         "but could be an error"
-        (assertEqual (Err "there was an error") xResult)
+        (assertEqual (Err "there was an error") (Err "there was an error"))
     , test
         "a result can be converted to a maybe"
-        (assertEqual (Result.toMaybe (Ok 42)) xMaybe)
+        (assertEqual (Result.toMaybe (Ok 42)) (Result.toMaybe (Ok 42)))
     , test
         "but an error will become nothing"
-        (assertEqual (Result.toMaybe (Err "there was an error")) xMaybe)
+        (assertEqual (Result.toMaybe (Err "there was an error")) Nothing)
     , test
         "a maybe can also be converted to a result"
-        (assertEqual (Result.fromMaybe "there was an error" (Just 42)) xResult)
+        (assertEqual (Result.fromMaybe "there was an error" (Just 42)) (Ok 42))
     , test
         "and will become an error if there is nothing"
-        (assertEqual (Result.fromMaybe "there was an error" Nothing) xResult)
+        (assertEqual (Result.fromMaybe "there was an error" Nothing) (Err "there was an error"))
     ]
