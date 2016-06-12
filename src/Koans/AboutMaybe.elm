64e8cdcc4741f01b14d024c57dbc6a27f09b2d7e
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 17:19:57 -0700

    [complete] about maybe

--- src/Koans/AboutMaybe.elm
+++ src/Koans/AboutMaybe.elm
@@ -10,26 +10,26 @@ testSuite =
     "About Maybe"
     [ test
         "maybe represents a value that may be nothing"
-        (assertEqual Nothing xMaybe)
+        (assertEqual Nothing Nothing)
     , test
         "or the value could be something"
-        (assertEqual (Just 5) xMaybe)
+        (assertEqual (Just 5) (Just 5))
     , test
         "withDefault can be used to get the value from a maybe"
-        (assertEqual (Maybe.withDefault 3 (Just 5)) xNum)
+        (assertEqual (Maybe.withDefault 3 (Just 5)) 5)
     , test
         "but will give the default value if there is nothing"
-        (assertEqual (Maybe.withDefault 3 Nothing) xNum)
+        (assertEqual (Maybe.withDefault 3 Nothing) 3)
     , test
         "oneOf will get the first value from a list of maybes"
-        (assertEqual (Maybe.oneOf [ Nothing, (Just 1), (Just 2) ]) xMaybe)
+        (assertEqual (Maybe.oneOf [ Nothing, (Just 1), (Just 2) ]) (Just 1))
     , test
         "but you could still wind up with nothing"
-        (assertEqual (Maybe.oneOf [ Nothing, Nothing, Nothing ]) xMaybe)
+        (assertEqual (Maybe.oneOf [ Nothing, Nothing, Nothing ]) Nothing)
     , test
         "map will transform the value in a maybe"
-        (assertEqual (Maybe.map (\n -> n / 2) (Just 4)) xMaybe)
+        (assertEqual (Maybe.map (\n -> n / 2) (Just 4)) (Just 2))
     , test
         "but will not transform a nothing"
-        (assertEqual (Maybe.map (\n -> n / 2) Nothing) xMaybe)
+        (assertEqual (Maybe.map (\n -> n / 2) Nothing) Nothing)
     ]
