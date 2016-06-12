16bd94b4878eb3ac1c788671cfc62efda2d27113
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 19:08:17 -0700

    [complete] about regex

--- src/Koans/AboutRegexes.elm
+++ src/Koans/AboutRegexes.elm
@@ -11,32 +11,32 @@ testSuite =
     "About Regex"
     [ test
         "contains tests for a regex match"
-        (assertEqual (Regex.contains (Regex.regex "[abc]+") "abcdefg") xBool)
+        (assertEqual (Regex.contains (Regex.regex "[abc]+") "abcdefg") True)
     , test
         "escape will escape all special characters"
-        (assertEqual (Regex.escape "[abc]+") xString)
+        (assertEqual (Regex.escape "[abc]+") "\\[abc\\]\\+")
     , test
         "useful for matching strange strings"
-        (assertEqual (Regex.contains (Regex.regex (Regex.escape "[eir]+")) "w[eir]+d") xBool)
+        (assertEqual (Regex.contains (Regex.regex (Regex.escape "[eir]+")) "w[eir]+d") True)
     , test
         "it's easy to make your regex case insensitive"
-        (assertEqual (Regex.contains (Regex.caseInsensitive (Regex.regex "abc")) "ABC") xBool)
+        (assertEqual (Regex.contains (Regex.caseInsensitive (Regex.regex "abc")) "ABC") True)
     , test
         "find returns a list of all matches"
-        (assertEqual (Regex.find Regex.All (Regex.regex "abc") "abcabcabc" |> List.length) xNum)
+        (assertEqual (Regex.find Regex.All (Regex.regex "abc") "abcabcabc" |> List.length) 3)
     , test
         "matches contain the match"
-        (assertEqual (Regex.find Regex.All (Regex.regex "[abc]") "axbxc" |> List.map (\match -> match.match)) xList)
+        (assertEqual (Regex.find Regex.All (Regex.regex "[abc]") "axbxc" |> List.map (\match -> match.match)) [ "a", "b", "c" ])
     , test
         "matches contain the index"
-        (assertEqual (Regex.find Regex.All (Regex.regex "[abc]") "axbxc" |> List.map (\match -> match.index)) xList)
+        (assertEqual (Regex.find Regex.All (Regex.regex "[abc]") "axbxc" |> List.map (\match -> match.index)) [ 0, 2, 4 ])
     , test
         "matches may contain submatches"
-        (assertEqual (Regex.find Regex.All (Regex.regex "(a)|(b)") "axbxc" |> List.map (\match -> match.submatches)) xList)
+        (assertEqual (Regex.find Regex.All (Regex.regex "(a)|(b)") "axbxc" |> List.map (\match -> match.submatches)) [ [ Just "a", Nothing ], [ Nothing, Just "b" ] ])
     , test
         "you may limit the number of matches"
-        (assertEqual (Regex.find (Regex.AtMost 2) (Regex.regex "[abc]") "axbxc" |> List.map (\match -> match.match)) xList)
+        (assertEqual (Regex.find (Regex.AtMost 2) (Regex.regex "[abc]") "axbxc" |> List.map (\match -> match.match)) [ "a", "b" ])
     , test
         "replace can modify based on the match"
-        (assertEqual (Regex.replace Regex.All (Regex.regex "[abc]") (\match -> String.toUpper match.match) "axbxc") xString)
+        (assertEqual (Regex.replace Regex.All (Regex.regex "[abc]") (\match -> String.toUpper match.match) "axbxc") "AxBxC")
     ]
